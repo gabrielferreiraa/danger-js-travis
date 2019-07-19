2058c543b4f7f46a24001827c8a91076abe65355
Gabriel Ferreira <gabriel.ferreira@iclinic.com.br>
2019-07-19 15:25:29 -0300

    first commit

---
+++ dangerfile.js
@@ -0,0 +1,15 @@
+import { fail, message, danger } from "danger";
+
+const modifiedMD = danger.git.modified_files.join("- ");
+message("Changed Files in this PR: \n - " + modifiedMD);
+
+function refBranchNameIsCorrect(branchName) {
+  return /^(feature|bugfix|hotfix|rt|build|enhancement)\/(\S+)$/.test(
+    branchName
+  );
+}
+
+const refBranch = (!!danger.github && danger.github.pr.head.ref) || false;
+if (refBranch && !refBranchNameIsCorrect(refBranch)) {
+  fail(`The branch **${refBranch}** is out of the pattern`);
+}
