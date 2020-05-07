42480f1324500cafbcf97f7c59382b4a164ef7fa
Gabriel Ferreira <gabriel.ferreira@iclinic.com.br>
2020-05-07 13:36:44 -0300

    fixa

--- dangerfile.js
+++ dangerfile.js
@@ -13,3 +13,8 @@ const refBranch = (!!danger.github && danger.github.pr.head.ref) || false;
 if (refBranch && !refBranchNameIsCorrect(refBranch)) {
   fail(`The branch **${refBranch}** is out of the pattern`);
 }
+
+const merges = danger.git.commits.filter(commit => commit.message.include("Merge Master"))
+if (merges.length) {
+  fail("Please rebase your PR")
+}
